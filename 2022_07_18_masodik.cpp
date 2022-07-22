e556f143887b9fa2312ffe9be16e3983e5c1113e
adamszanto <51482667+adamszanto@users.noreply.github.com>
2022-07-22 21:44:50 +0200

    Create 2022_07_18_masodik.cpp

---
+++ 2022_07_18_masodik.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+template<typename T> 
+int maximum(T* tomb, int jobb)
+{
+    if (jobb == 0)
+    {
+        return 0;
+    }
+
+
+}
+
+int main()
+{
+    
+}
